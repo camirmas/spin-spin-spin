9de6531a86d1982733076dbca05647018ccafd9a
Cameron Irmas <cjirmas@gmail.com>
2019-11-18 14:38:06 -0800

    Add Ellipsis loader

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,4 +2,5 @@ declare module 'loading' {
   export function DualRing(): JSX.Element;
   export function Circle(): JSX.Element;
   export function Default(): JSX.Element;
+  export function Ellipsis(): JSX.Element;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import DualRing from './components/DualRing';
 import Circle from './components/Circle';
 import Default from './components/Default';
+import Ellipsis from './components/Ellipsis';
 
-export { DualRing, Circle, Default };
+export { DualRing, Circle, Default, Ellipsis };
