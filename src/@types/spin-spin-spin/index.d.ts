62ab311931d4ab28fd1e57e48979c37c5c5121a4
Cam <cirmas@protonmail.com>
2019-11-18 17:19:30 -0800

    Update version

---
+++ src/@types/spin-spin-spin/index.d.ts
@@ -0,0 +1,14 @@
+declare module 'spin-spin-spin' {
+  export function DualRing(): JSX.Element;
+  export function Circle(): JSX.Element;
+  export function Default(): JSX.Element;
+  export function Ellipsis(): JSX.Element;
+  export function Facebook(): JSX.Element;
+  export function Grid(): JSX.Element;
+  export function Heart(): JSX.Element;
+  export function Hourglass(): JSX.Element;
+  export function Ring(): JSX.Element;
+  export function Ripple(): JSX.Element;
+  export function Roller(): JSX.Element;
+  export function Spinner(): JSX.Element;
+}
